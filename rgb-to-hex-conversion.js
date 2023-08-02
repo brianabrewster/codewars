7ed3000ca0e2b9d746fd04d7a1ec307d1a3d30ef
Briana <briana_brewster4@yahoo.com>
2023-08-02 12:09:18 -0500

    new codewars challenge

---
+++ rgb-to-hex-conversion.js
@@ -0,0 +1,13 @@
+// The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned. Valid decimal values for RGB are 0 - 255. Any values that fall out of that range must be rounded to the closest valid value.
+
+// Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+
+function rgb(r, g, b){
+	return toHex(r)+toHex(g)+toHex(b);
+}
+
+function toHex(d) {
+    if(d < 0 ) {return "00";}
+    if(d > 255 ) {return "FF";}
+    return  ("0"+(Number(d).toString(16))).slice(-2).toUpperCase()
+}
